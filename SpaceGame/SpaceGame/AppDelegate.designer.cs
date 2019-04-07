b620312e6a450d11ff0fe389c625e893e472392c
Vladislav Khudziakov <vladislav.khudiakov@banuba.com>
2019-04-07 16:37:28 +0300

    add start files to project (initial commit

---
+++ SpaceGame/SpaceGame/AppDelegate.designer.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace SpaceGame
+{
+  [Register("AppDelegate")]
+  partial class AppDelegate
+  {
+    [Outlet]
+    SpriteKit.SKView MyGameView { get; set; }
+
+    void ReleaseDesignerOutlets()
+    {
+      if (MyGameView != null)
+      {
+        MyGameView.Dispose();
+        MyGameView = null;
+      }
+    }
+  }
+}
